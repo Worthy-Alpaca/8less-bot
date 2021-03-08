f2aeb2a5d5fc41faaf489090bab7faec43ca497c
Stephan Schumacher aka Worthy Alpaca <worthyalpaca@gmail.com>
2021-03-09 00:43:34 +0100

    added env functionality

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const fs = require("fs");
 const Discord = require('discord.js');
 const client = new Discord.Client();
 const { DISCORD_TOKEN } = require('./config.json');
-
+require('dotenv').config();
 const prefix = "!";
 
 client.commands = new Discord.Collection();
@@ -56,4 +56,4 @@ client.on('message', message => {
     cmd.execute(client, message, args);
 });
 
-client.login(DISCORD_TOKEN);
+client.login(process.env.TOKEN)
